ae13572e29a59c63b48fe2db87b78c8f5d5dc13a
gastudillo <47458556+gabrielastudillo@users.noreply.github.com>
2026-02-21 08:17:02 -0500

    Update swtich_enum_class.ino

--- week_3/swtich_enum_class.ino
+++ week_3/swtich_enum_class.ino
@@ -1,20 +1,40 @@
-enum class Mode : uint8_t { Slow, Medium, Fast };
+enum class Mode : uint8_t { Off, On, Blink };
 
-Mode mode = Mode::Medium;
+Mode mode = Mode::Off;
 
-void applyMode(Mode m) {
-  switch (m) {
-    case Mode::Slow:
-      Serial.println("Mode: Slow (800 ms)");
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  switch (mode) {
+    case Mode::Off:
+      digitalWrite(LED_BUILTIN, LOW);
       break;
-    case Mode::Medium:
-      Serial.println("Mode: Medium (400 ms)");
+
+    case Mode::On:
+      digitalWrite(LED_BUILTIN, HIGH);
       break;
-    case Mode::Fast:
-      Serial.println("Mode: Fast (120 ms)");
+
+    case Mode::Blink: {
+      static unsigned long last = 0;
+      static bool led = false;
+      unsigned long now = millis();
+      if (now - last >= 300) {
+        led = !led;
+        digitalWrite(LED_BUILTIN, led ? HIGH : LOW);
+        last = now;
+      }
       break;
-    default:
-      // normally unreachable if all cases covered
-      break;
+    }
+  }
+
+  // Example: change mode from Serial
+  if (Serial.available()) {
+    char c = Serial.read();
+    if (c == 'o') mode = Mode::On;
+    else if (c == 'f') mode = Mode::Off;
+    else if (c == 'b') mode = Mode::Blink;
   }
 }
