82e4a4c084a75d82c3668ea9aa39438582845e43
gastudillo <47458556+gabrielastudillo@users.noreply.github.com>
2026-04-07 17:10:19 -0400

    Add files via upload

---
+++ week_8/real_time_and_summary.ino
@@ -0,0 +1,60 @@
+#include <Arduino_MKRENV.h>
+
+const int N = 10;
+float temps[N];
+int indexPos = 0;
+
+const float ALERT_TEMP = 26.0;  // check your room temp
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!ENV.begin()) {
+    Serial.println("ENV Shield not detected!");
+    while (true) { }
+  }
+
+  Serial.println("Starting...");
+}
+
+void loop() {
+  float t = ENV.readTemperature();  // collected data
+  temps[indexPos] = t;
+  indexPos++;
+
+  // REAL-TIME REPORT (only when needed)
+  if (t >= ALERT_TEMP) {
+    Serial.print("!!! REAL-TIME ALERT temp=");
+    Serial.println(t);
+  }
+
+  delay(1000);
+
+  // SUMMARY REPORT every N samples
+  if (indexPos == N) {
+    Serial.print("SUMMARY avg=");
+    Serial.print(average(temps, N));
+    Serial.print(" min=");
+    Serial.print(minimum(temps, N));
+    Serial.print(" max=");
+    Serial.println(maximum(temps, N));
+
+    indexPos = 0;
+  }
+}
+
+float average(float arr[], int size) {
+  float sum = 0;
+  for (int i = 0; i < size; i++) sum += arr[i];
+  return sum / size;
+}
+float minimum(float arr[], int size) {
+  float mn = arr[0];
+  for (int i = 1; i < size; i++) if (arr[i] < mn) mn = arr[i];
+  return mn;
+}
+float maximum(float arr[], int size) {
+  float mx = arr[0];
+  for (int i = 1; i < size; i++) if (arr[i] > mx) mx = arr[i];
+  return mx;
+}
